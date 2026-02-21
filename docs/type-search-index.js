db263ecaeb97b8445f8fd3a94ed1248cbfc01d18
Anam Giri <anamgiri@Anams-MacBook-Air.local>
2026-02-20 19:32:43 -0600

    Add Javadoc documentation

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"FeatureRestock"},{"p":"<Unnamed>","l":"GroceryManager"},{"p":"<Unnamed>","l":"Inventory"}];updateSearchResults();
